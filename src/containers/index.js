267df96397f5f6027138675f632d62199b0afd6f
codingBryan <bryanwere174@gmail.com>
2022-07-15 07:03:27 +0300

    initial

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blogs} from './blog/Blogs';
+export {default as Features} from './features/Features';
+export {default as Footer} from './footer/Footer';
+export {default as WhatGPT} from './whatGPT/WhatGPT';
+export {default as Header} from './header/Header';
+export {default as Possibilty} from './possibility/Possibility';
---
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './WhatGPT.css'
+
+const WhatGPT = () => {
+  return (
+    <div>WhatGPT</div>
+  )
+}
+
+export default WhatGPT
